675a84b915abc8cd67e1ea108c2d9c178fe08880
Osman Baş <obas1869@gmail.com>
2025-05-24 02:10:09 +0300

    Sorular için özellik tanımlama.

--- Quizzer/Question.swift
+++ Quizzer/Question.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct Question: Identifiable, Codable {
+    let id: Int
+    let question: String
+    let options: [String]
+    let correctAnswerIndex: Int
+    let timeLimit: Int // saniye cinsinden
+}
+
