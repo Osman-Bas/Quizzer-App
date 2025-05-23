9c3698312ddda64d6ab46995012d6f34204d6d53
Osman Baş <obas1869@gmail.com>
2025-05-24 02:11:10 +0300

    DEBUG: JSON'dan gelen soruları test etmek için kullanıldı.

--- Quizzer/QuestionListView.swift
+++ Quizzer/QuestionListView.swift
@@ -5,13 +5,33 @@
 //  Created by Osman Baş on 24.05.2025.
 //
 
+
+// DEBUG: JSON'dan gelen soruları test etmek için kullanıldı
+
+
 import SwiftUI
 
 struct QuestionListView: View {
+    let questions = QuestionLoader.loadQuestions()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(questions) { question in
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(question.question)
+                        .font(.headline)
+                    ForEach(question.options.indices, id: \.self) { index in
+                        Text("- \(question.options[index])")
+                            .font(.subheadline)
+                    }
+                }
+                .padding(.vertical, 8)
+            }
+            .navigationTitle("Tüm Sorular")
+        }
     }
 }
+
 
 #Preview {
     QuestionListView()
