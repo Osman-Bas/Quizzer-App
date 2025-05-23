c0c4c7c456ef71dadef2ac8860cf38fc73980c9c
Osman Baş <obas1869@gmail.com>
2025-05-24 01:28:56 +0300

    Initial Commit

---
+++ Quizzer/QuizzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzerApp.swift
+//  Quizzer
+//
+//  Created by Osman Baş on 24.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizzerTests/QuizzerTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzerTests.swift
+//  QuizzerTests
+//
+//  Created by Osman Baş on 24.05.2025.
+//
+
+import Testing
+@testable import Quizzer
+
+struct QuizzerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
